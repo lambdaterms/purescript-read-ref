dde8913cf0718ca8488e747edfd1e36578ee8056
Tomasz Rybarczyk <paluho@gmail.com>
2018-09-05 22:10:04 +0200

    Simplify example

--- src/Main.purs
+++ src/Main.purs
@@ -14,11 +14,7 @@ foreign import data ReadRef ∷ Type → Type
 
 newtype RefMutation h a = RefMutation ((a → a) → Effect a)
 
--- | Create a read-only reference containing specified value,
--- | and a mutating function
 foreign import new ∷ ∀ h a. a → Effect {ref ∷ ReadRef a, mutation ∷ RefMutation h a}
-
--- | Read the current value of a reference
 foreign import read ∷ ∀ a. ReadRef a → Effect a
 
 newRefMutator ∷ ∀ a b. a → (∀ h. RefMutation h a → b) → Effect {ref ∷ ReadRef a, mutator ∷ b}
@@ -26,33 +22,19 @@ newRefMutator val mut = do
   {ref, mutation} ← new val
   pure { ref, mutator: (unsafeCoerce mut) mutation }
 
--- foreign import coerceMutator ∷ ∀ a. (∀ h. RefMutator h ⇒ a) → a
-
 mutateRef ∷ ∀ h a. RefMutation h a → (a → a) → Effect a
 mutateRef (RefMutation x) f = log "mutating" *> x f
 
--- logEight ∷ (RefMutation "TEST" Int) → Effect Unit
-logEight (RefMutation refMutation1) = do -- (RefMutation rM2) = do -- (RefMutation refMutation2) = do
-  x ← refMutation1 (const 8)
-  log "log"
-  log (show x)
-  pure unit
-
--- logEight ∷ (RefMutation "TEST" Int) → Effect Unit
-badLogEight (RefMutation refMutation1) (RefMutation rM2) = do -- (RefMutation refMutation2) = do
-  x ← refMutation1 (const 8)
-  log "log"
-  log (show x)
-  pure unit
+mutatingComponent r = r
+  -- XXX: you are not able to return r
 
 
 main ∷ Effect Unit
 main = do
-  { ref, mutator } ∷ _ ← newRefMutator 8 logEight
-  mutator
-  { ref: ref2, mutator: mutator2 } ∷ _ ← newRefMutator 8 badLogEight
-  -- { ref, mutator: m } ← newRefMutator "STRING" mutator2
-  -- m
+  { ref, mutator } ∷ _ ← newRefMutator 8 mutatingComponent
+
+  traceM mutator
+
   log "BLBL"
   pure unit
 
